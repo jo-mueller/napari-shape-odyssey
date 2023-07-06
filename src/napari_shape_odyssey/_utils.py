40c2cce9f374b0416a515694dc2d276443c51c67
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2023-07-06 16:32:31 +0200

    added some mesh format conversion utilities

---
+++ src/napari_shape_odyssey/_utils.py
@@ -0,0 +1,40 @@
+import vedo
+
+
+def _vedo_to_trimesh(mesh: vedo.Mesh):
+    """Convert a vedo mesh to a trimesh mesh.
+
+    Args:
+        mesh (vedo.Mesh): A vedo mesh.
+
+    Returns:
+        trimesh.Trimesh: A trimesh mesh.
+    """
+    from pyFM.mesh import TriMesh
+
+    return TriMesh(mesh.points(), mesh.faces())
+
+
+def _surfacetuple_to_trimesh(mesh: "napari.types.SurfaceData"):
+    """Convert a napari surface tuple to a trimesh mesh.
+
+    Args:
+        mesh (napari.types.SurfaceData): A napari surface tuple.
+
+    Returns:
+        trimesh.Trimesh: A trimesh mesh.
+    """
+    return _vedo_to_trimesh(vedo.Mesh([mesh[0], mesh[1]]))
+
+
+def _vedo_to_tuple(mesh: vedo.Mesh):
+    """Convert a vedo mesh to a napari surface tuple.
+
+    Args:
+        mesh (vedo.Mesh): A vedo mesh.
+
+    Returns:
+        napari.types.SurfaceData: A napari surface tuple.
+    """
+    import numpy as np
+    return (mesh.points(), np.asarray(mesh.faces()))
