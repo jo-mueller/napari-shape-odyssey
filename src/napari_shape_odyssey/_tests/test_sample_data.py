e72fa36d284c9cc474c75970b5b90b5d6b20ecb5
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2023-07-07 18:54:02 +0200

    removed example tests

--- src/napari_shape_odyssey/_tests/test_sample_data.py
+++
@@ -1,7 +0,0 @@
-# from napari_shape_odyssey import make_sample_data
-
-# add your tests here...
-
-
-def test_something():
-    pass
--- src/napari_shape_odyssey/_tests/test_writer.py
+++
@@ -1,7 +0,0 @@
-# from napari_shape_odyssey import write_single_image, write_multiple
-
-# add your tests here...
-
-
-def test_something():
-    pass
