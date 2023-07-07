df97cb6cdd5e1f1958b655dc95f005828a22f812
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2023-07-07 19:15:44 +0200

    fixed test

--- src/napari_shape_odyssey/_tests/test_spectral.py
+++ src/napari_shape_odyssey/_tests/test_spectral.py
@@ -1,13 +1,16 @@
-def test_shape_DNA(make_napari_viewer):
+def test_shape_fingerprint(make_napari_viewer):
     import vedo
     import numpy as np
-    from .._spectral import _shape_DNA
+    from .._spectral import _shape_fingerprint
     from napari.layers import Layer
 
     sample_data = vedo.load(vedo.dataurl + "bunny.obj")
     viewer = make_napari_viewer()
 
     mesh_tuple = (sample_data.points(), np.asarray(sample_data.faces()))
-    result = _shape_DNA(mesh_tuple, order=100, robust=False)
+    result = _shape_fingerprint(mesh_tuple, order=100, robust=False)
+
+    layer = layer.create(result[0], result[1], result[2])
+    viewer.add_layer(layer)
 
     
