1b1cf9d12427227eb27f6096b019b4e5e0ea0396
Johannes Müller <38459088+jo-mueller@users.noreply.github.com>
2023-08-25 11:46:32 +0200

    added unwrapping

--- src/napari_shape_odyssey/_unwrapping/__init__.py
+++ src/napari_shape_odyssey/_unwrapping/__init__.py
@@ -1,2 +1,2 @@
-from .unwrap import map_surface_to_sphere
+from .unwrap import map_surface_to_sphere, map_surface_to_disk
 from .projections import mercator_projection
--- src/napari_shape_odyssey/_unwrapping/unwrap.py
+++ src/napari_shape_odyssey/_unwrapping/unwrap.py
@@ -1,6 +1,67 @@
 from napari_tools_menu import register_function
 
 
+def map_surface_to_disk(surface: "napari.types.SurfaceData",
+                        only_uvs: bool = True,
+                        force_disk: bool = True,
+                        ) -> "napari.types.SurfaceData":
+
+    """
+    Map a surface to a disk.
+    Parameters
+    ----------
+    surface : napari.types.SurfaceData
+        The surface to be mapped to a disk.
+    only_uvs : bool
+        If True, only the texture coordinates are returned.
+    Returns
+    -------
+    napari.types.SurfaceData
+        The mapped surface.
+    """
+    import os
+    from pathlib import Path
+    import vedo
+    import numpy as np
+    import warnings
+
+    do_uvs = ' --writeOnlyUVs' if only_uvs else ''
+    force_disk = ' --flattenToDisk' if force_disk else ''
+
+    local_directory = Path(__file__).parent.absolute()
+    executable = _install_bff_unwrapping()
+
+    center_of_mass = surface[0].mean(axis=0)
+
+    mesh_in = vedo.Mesh((surface[0] - center_of_mass[None, :], surface[1]))
+    surface_area_in = mesh_in.area()
+    vedo.write(mesh_in, os.path.join(local_directory, 'mesh_in.obj'))
+
+    # run bff commandline executable
+    os.system(f'{executable} ' +
+              f'{os.path.join(local_directory, "mesh_in.obj")} ' +
+              f'{os.path.join(local_directory, "mesh_out.obj")} ' +
+              f'{force_disk}{do_uvs}')
+
+    # read output mesh
+    mesh_out = vedo.load(os.path.join(local_directory, 'mesh_out.obj')).clean()
+    surface_area_out = mesh_out.area()
+
+    # calculate new points
+    scale_factor = np.sqrt(surface_area_in / surface_area_out)
+    points = mesh_out.points() * scale_factor + center_of_mass[None, :]
+    faces = np.asarray(mesh_out.faces())
+
+    if len(mesh_out.points()) != len(surface[0]):
+        warnings.warn(
+            "The number of vertices in the output mesh is different from the input mesh. " +
+            "This can happen if the input mesh is not a closed surface. " +
+            "The output mesh will be returned without the texture coordinates.")
+        return (points, faces)
+    else:
+        return (points, faces, surface[2])
+
+
 @register_function(menu="Surfaces > Map mesh to sphere (nso, bff)")
 def map_surface_to_sphere(surface: "napari.types.SurfaceData"
                           ) -> "napari.types.SurfaceData":
@@ -36,7 +97,7 @@ def map_surface_to_sphere(surface: "napari.types.SurfaceData"
               '--mapToSphere')
 
     # read output mesh
-    mesh_out = vedo.load(os.path.join(local_directory, 'mesh_out.obj'))
+    mesh_out = vedo.load(os.path.join(local_directory, 'mesh_out.obj')).clean()
     surface_area_out = mesh_out.area()
 
     # calculate new points
@@ -51,8 +112,7 @@ def map_surface_to_sphere(surface: "napari.types.SurfaceData"
             "The output mesh will be returned without the texture coordinates.")
         return (points, faces)
     else:
-        return (points, faces, 
-        surface[2])
+        return (points, faces, surface[2])
 
 
 def _install_bff_unwrapping():
